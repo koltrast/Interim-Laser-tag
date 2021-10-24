b81b2e2fd4e79dfc504a2f834214ecea90dcb5fc
koltrast <koltrast@pm.me>
2021-10-25 00:37:40 +0200

    modifié :         scanazon/main.py nigthly update

--- scanazon/main.py
+++ scanazon/main.py
@@ -1,6 +1,5 @@
-#coding:utf8
+#!/usr/bin/env python
 
-# dictionnary
 orig_list = ["A01", "A02", "A03", "A04", "B01", "B02", "B03", "B04", "C01", "C02", "C03", "C04", "D01", "D02", "D03", "D04", "E01", "E02", "E03", "E04", "F01", "F02", "F03", "F04", "G01", "G02", "G03", "G04", "I01", "I02", "I03", "I04", "J01", "J02", "J03", "J04", "K01", "K02", "K03", "K04", "L01", "L02", "L03", "L04", "M01", "M02", "M03", "M04", "N01", "N02", "N03", "N04", "O01", "O02", "O03", "O04", "P01", "P02", "P03", "P04", "Q01", "Q02", "Q03", "Q04", "R01", "R02", "R03", "R04", "S01", "S02", "S03", "S04", "T01", "T02", "T03", "T04", "U01", "U02", "U03", "U04", "V01", "V02", "V03", "V04", "W01", "W02", "W03", "W04", "X01", "X02"]
 
 
@@ -34,17 +33,20 @@ while a == 1:
     if input() == "START":
         t0 = time.time()
         i = 0
-        while i < n:
+        b = 1
+        while i < n :
             print(str(n - i) + " to scan")
             scan = input()
             if scan in sample_list:
                 print(scan, "is in the list")
                 sample_list.remove(scan)
                 i = i + 1
+            elif scan == "STOP":
+                print("This can’t stop")
             else:
                 penalities()
                 print("You dumb idiot")
-        while a ==1:
+        while a ==1 and b !=0:
             print("Scan the stop bar code")
             if input() == "STOP":
                 t1 = time.time()
@@ -58,3 +60,4 @@ while a == 1:
                 a = 1
     else:
         a = 1
+        b = 1
